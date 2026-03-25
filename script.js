14a564c35400bdc6d104a1984af6f578bf55cb70
blackcatdevs1 <blackcatdevs@protonmail.com>
2026-03-25 07:53:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// CUSTOM CURSOR
 const cursor = document.querySelector(".cursor");
 
-document.addEventListener("mousemove",(e)=>{
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
 
-cursor.style.left = e.clientX + "px";
-cursor.style.top = e.clientY + "px";
 
+// MOUSE FOLLOW GLOW
+const cards = document.querySelectorAll(".info-box");
+
+cards.forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    card.style.setProperty("--x", x + "%");
+    card.style.setProperty("--y", y + "%");
+  });
 });
